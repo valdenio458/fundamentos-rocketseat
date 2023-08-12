e5cbd25b9b0ce0be0164701c46d40af45417068f
valdenio458 <valdeniomartinho@gmail.com>
2023-08-12 16:17:46 -0300

    adiciona a lógica do mode no script

---
+++ script.js
@@ -0,0 +1,13 @@
+const toogleMode = () => {
+  const html = document.documentElement;
+  html.classList.toggle('light')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img");
+
+  if(html.classList.contains('light')){
+    img.setAttribute('src', './assets/avatar-light.JPG')
+  } else {
+    img.setAttribute('src', './assets/avatar.png') 
+  }
+}
