71590b14a67d3362b9abb03f6ceb8d817559717e
valdenio458 <valdeniomartinho@gmail.com>
2023-08-12 20:43:07 -0300

    comenta avatar

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const toogleMode = () => {
   html.classList.toggle('light')
 
   // pegar a tag img
-  const img = document.querySelector("#profile img");
+  // const img = document.querySelector("#profile img");
 
-  if(html.classList.contains('light')){
-    img.setAttribute('src', './assets/avatar-light.JPG')
-  } else {
-    img.setAttribute('src', './assets/avatar.png') 
-  }
+  // if(html.classList.contains('light')){
+  //   img.setAttribute('src', './assets/avatar-light.JPG')
+  // } else {
+  //   img.setAttribute('src', './assets/avatar.png') 
+  // }
 }
